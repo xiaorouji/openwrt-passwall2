942c32f8554f27508a4934ad575b7c84114caff4
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2023-09-03 01:07:14 +0800

    luci: fix subscribe script typo

--- luci-app-passwall2/root/usr/share/passwall2/subscribe.lua
+++ luci-app-passwall2/root/usr/share/passwall2/subscribe.lua
@@ -299,8 +299,10 @@ do
 				end
 			end
 		else
-			if v.currentNode == nil and v.delete then
-				v.delete()
+			if v.currentNode == nil then
+				if v.delete then
+					v.delete()
+				end
 				CONFIG[k] = nil
 			end
 		end
@@ -793,7 +795,7 @@ local function truncate_nodes(add_from)
 			end
 			config.set(config)
 		else
-			if config.currentNode.add_mode == "2" then
+			if config.currentNode and config.currentNode.add_mode == "2" then
 				if add_from then
 					if config.currentNode.add_from and config.currentNode.add_from == add_from then
 						config.set(config, "nil")
@@ -933,7 +935,7 @@ local function select_node(nodes, config)
 			config.set(config, server)
 		end
 	else
-		config.set(config, nil)
+		config.set(config, "nil")
 	end
 end
 
