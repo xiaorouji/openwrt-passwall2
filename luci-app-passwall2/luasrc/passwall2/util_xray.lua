3a816f2627ba912c1a5c585321f8bac9f7ce1c0a
xiaorouji <60100640+xiaorouji@users.noreply.github.com>
2024-04-12 00:11:10 +0800

    luci: fix #553

--- luci-app-passwall2/luasrc/passwall2/util_xray.lua
+++ luci-app-passwall2/luasrc/passwall2/util_xray.lua
@@ -1114,6 +1114,8 @@ function gen_config(var)
 				end
 			end)
 		end
+
+		local _remote_dns_ip = nil
 	
 		local _remote_dns = {
 			_flag = "remote",
@@ -1124,12 +1126,14 @@ function gen_config(var)
 			_remote_dns.address = remote_dns_udp_server
 			_remote_dns.port = tonumber(remote_dns_udp_port) or 53
 			_remote_dns_proto = "udp"
+			_remote_dns_ip = remote_dns_udp_server
 		end
 
 		if remote_dns_tcp_server then
 			_remote_dns.address = "tcp://" .. remote_dns_tcp_server
 			_remote_dns.port = tonumber(remote_dns_tcp_port) or 53
 			_remote_dns_proto = "tcp"
+			_remote_dns_ip = remote_dns_tcp_server
 		end
 
 		if remote_dns_doh_url and remote_dns_doh_host then
@@ -1138,6 +1142,7 @@ function gen_config(var)
 			end
 			_remote_dns.address = remote_dns_doh_url
 			_remote_dns.port = tonumber(remote_dns_doh_port) or 443
+			_remote_dns_ip = remote_dns_doh_ip
 		end
 
 		if _remote_dns.address then
@@ -1146,7 +1151,7 @@ function gen_config(var)
 				table.insert(routing.rules, 1, {
 					type = "field",
 					ip = {
-						_remote_dns.address
+						_remote_dns_ip
 					},
 					port = _remote_dns.port,
 					network = _remote_dns_proto,
